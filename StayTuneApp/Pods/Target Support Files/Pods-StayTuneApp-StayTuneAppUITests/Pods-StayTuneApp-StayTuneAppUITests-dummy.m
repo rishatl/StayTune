079d943de67b26adedf3a403e5f57c56d126e590
Rishat <rishatl@inbox.ru>
2021-03-17 17:49:03 +0300

    Update_Added_Authorization
    Without Pods

--- StayTuneApp/Pods/Target Support Files/Pods-StayTuneApp-StayTuneAppUITests/Pods-StayTuneApp-StayTuneAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StayTuneApp_StayTuneAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_StayTuneApp_StayTuneAppUITests
-@end
--- StayTuneApp/Pods/Target Support Files/Pods-StayTuneApp/Pods-StayTuneApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StayTuneApp : NSObject
-@end
-@implementation PodsDummy_Pods_StayTuneApp
-@end
